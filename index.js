a862b4a1835ad8396a006d1d8629be9b053e638f
perfectgech-source <perfectgech@gmail.com>
2026-09-08 19:28:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
+const ADMIN_ID = 562673622;
 bot.start((ctx) => {
   ctx.reply(
     "እንኳን ወደ Tsion Tasks Bot በደህና መጡ!",
