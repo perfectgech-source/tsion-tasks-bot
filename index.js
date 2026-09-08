8a872d700f926bb450d109d6ac5c23f08bc427da
perfectgech-source <perfectgech@gmail.com>
2026-09-08 19:42:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Telegraf, Markup } = require("telegraf");
+uconst { Telegraf, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const ADMIN_ID = 562673622;
@@ -23,6 +23,26 @@ bot.hears("👤 Profile", (ctx) => {
 
 bot.hears("📋 Tasks", (ctx) => {
   ctx.reply("No tasks available yet.");
+});bot.command("admin", (ctx) => {
+  if (ctx.from.id !== ADMIN_ID) {
+    return ctx.reply("Access denied");
+  }
+
+  ctx.reply(
+    "🔐 Admin Panel",
+    Markup.keyboard([
+      ["➕ Create Task"],
+      ["⏳ Pending Proofs"],
+      ["👥 Users"],
+      ["📊 Statistics"]
+    ]).resize()
+  );
+});
+
+bot.hears("➕ Create Task", (ctx) => {
+  if (ctx.from.id !== ADMIN_ID) return;
+
+  ctx.reply("Task Created!\n\n📌 Test Task\n💰 Reward: 5 ETB");
 });
 
 bot.launch();
